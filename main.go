a7e218c4377f236f12c63b63b8cf34471b7f8806
ma_dev <1341935532@qq.com>
2022-09-09 15:56:47 +0800

    Gin实践 连载六 Golang优雅重启HTTP服务

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	_ "gin_log/models"
 	"gin_log/pkg/setting"
 	"gin_log/routers"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
 	router := routers.InitRouter()
 	s := &http.Server{
-		Addr: fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler: router,
-		ReadTimeout: setting.ReadTimeout,
-		WriteTimeout: setting.WriteTimeout,
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-	_ = s.ListenAndServe()
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Println("listen:", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Server Shutdown")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Println("Shutdown:", err)
+	}
+
+	log.Println("Server Exit")
 }
